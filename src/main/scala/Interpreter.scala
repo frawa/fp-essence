abfa889c05054559dd39ab8dd3103cd3e67a7b49
Frank Wagner <agilecoderfrank@gmail.com>
2023-01-08 22:07:46 +0100

    variation one: E aka InterpreterE

--- src/main/scala/Interpreter.scala
+++ src/main/scala/Interpreter.scala
@@ -21,44 +21,77 @@ trait Interpreter {
   protected def unitM[A](v: A): M[A]
   protected def bindM[A, B](m: M[A])(f: A => M[B]): M[B]
 
-  def testTerm(term: Term): String = showM(interp(term, Seq()))
+  final def testTerm(term: Term): String = showM(interp(term, Seq()))
 
   private type Environment = Seq[(Name, Value)]
 
+  import Term._
+  import Value._
+
   protected def showval(v: Value): String = v match {
-    case Value.Wrong  => "<wrong>"
-    case Value.Num(i) => s"$i"
-    case Value.Fun(f) => "<function>"
+    case Wrong  => "<wrong>"
+    case Num(i) => s"$i"
+    case Fun(f) => "<function>"
   }
 
   private def interp(t: Term, e: Environment): M[Value] = t match {
-    case Term.Var(name) => lookup(name, e)
-    case Term.Con(i)    => unitM(Value.Num(i))
-    case Term.Add(a, b) => bindM(interp(a, e)) { a => bindM(interp(b, e)) { b => unitM(add(a, b)) } }
-    case Term.Lam(x, t) => unitM(Value.Fun(xx => interp(t, (x, xx) +: e)))
-    case Term.App(f, t) => bindM(interp(f, e)) { f => bindM(interp(t, e)) { t => apply(f, t) } }
+    case Var(name) => lookup(name, e)
+    case Con(i)    => unitM(Num(i))
+    case Add(a, b) => bindM(interp(a, e)) { a => bindM(interp(b, e)) { b => add(a, b) } }
+    case Lam(x, t) => unitM(Fun(xx => interp(t, (x, xx) +: e)))
+    case App(f, t) => bindM(interp(f, e)) { f => bindM(interp(t, e)) { t => apply(f, t) } }
   }
 
   private def lookup(name: Name, e: Environment): M[Value] = e match {
-    case Nil            => unitM(Value.Wrong)
+    case Nil            => wrong(s"unbound variable: $name")
     case (n, v) :: rest => if n == name then unitM(v) else lookup(name, rest)
   }
 
-  private def add(a: Value, b: Value): Value = (a, b) match {
-    case (Value.Num(a), Value.Num(b)) => Value.Num(a + b)
-    case _                            => Value.Wrong
+  private def add(a: Value, b: Value): M[Value] = (a, b) match {
+    case (Num(a), Num(b)) => unitM(Num(a + b))
+    case _                => wrong(s"should be numbers: ${showval(a)}, ${showval(b)}")
   }
 
   private def apply(f: Value, v: Value): M[Value] = f match {
-    case Value.Fun(f) => f(v)
-    case _            => unitM(Value.Wrong)
+    case Fun(f) => f(v)
+    case _      => wrong(s"should be function: ${showval(f)}")
   }
+
+  protected def wrong(message: String): M[Value] =
+    unitM(Wrong)
 
 }
 
 object InterpreterI extends Interpreter:
   type M[A] = A
 
-  protected def showM(m: M[Value]): String               = showval(m)
-  protected def unitM[A](v: A): M[A]                     = v
-  protected def bindM[A, B](m: M[A])(f: A => M[B]): M[B] = f(m)
+  override def showM(m: M[Value]): String               = showval(m)
+  override def unitM[A](v: A): M[A]                     = v
+  override def bindM[A, B](m: M[A])(f: A => M[B]): M[B] = f(m)
+
+object InterpreterE extends Interpreter:
+  type M[A] = E[A]
+
+  enum E[A]:
+    case Success(a: A)          extends E[A]
+    case Error(message: String) extends E[A]
+
+  override def showM(m: M[Value]): String               = showE(m)
+  override def unitM[A](v: A): E[A]                     = unitE(v)
+  override def bindM[A, B](m: E[A])(f: A => E[B]): E[B] = bindE(m)(f)
+
+  private def showE(m: E[Value]): String = m match {
+    case E.Success(a) => s"Success: ${showval(a)}"
+    case E.Error(m)   => s"Error: ${m}"
+  }
+
+  private def unitE[A](a: A): E[A]       = E.Success(a)
+  private def errorE[A](m: String): E[A] = E.Error(m)
+
+  private def bindE[A, B](m: E[A])(f: A => E[B]): E[B] = m match {
+    case E.Success(a) => f(a)
+    case E.Error(m)   => E.Error(m)
+  }
+
+  override def wrong(message: String): E[Value] =
+    errorE(message)
--- src/test/scala/InterpreterTest.scala
+++ src/test/scala/InterpreterTest.scala
@@ -9,12 +9,24 @@ class InterpreterTest extends munit.FunSuite {
   val term0 = App(Lam("x", Add(Var("x"), Var("x"))), Add(Con(10), Con(11)))
   val term1 = App(Con(1), Con(2))
 
-  test("42") {
+  test("I 42") {
     import InterpreterI._
     assertEquals(testTerm(term0), "42")
   }
-  test("wrong") {
+
+  test("I wrong") {
     import InterpreterI._
     assertEquals(testTerm(term1), "<wrong>")
   }
+
+  test("E 42") {
+    import InterpreterE._
+    assertEquals(testTerm(term0), "Success: 42")
+  }
+
+  test("E wrong") {
+    import InterpreterE._
+    assertEquals(testTerm(term1), "Error: should be function: 1")
+  }
+
 }
