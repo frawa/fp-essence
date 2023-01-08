37ffe9097b341413765698118b4cf548d4a8cd14
Frank Wagner <agilecoderfrank@gmail.com>
2023-01-08 21:41:48 +0100

    test for wrong

--- src/test/scala/InterpreterTest.scala
+++ src/test/scala/InterpreterTest.scala
@@ -6,9 +6,15 @@ import Term._
 // https://scalameta.org/munit/docs/getting-started.html
 
 class InterpreterTest extends munit.FunSuite {
+  val term0 = App(Lam("x", Add(Var("x"), Var("x"))), Add(Con(10), Con(11)))
+  val term1 = App(Con(1), Con(2))
+
   test("42") {
-    import InterpreterI.testTerm
-    val term0 = App(Lam("x", Add(Var("x"), Var("x"))), Add(Con(10), Con(11)))
+    import InterpreterI._
     assertEquals(testTerm(term0), "42")
+  }
+  test("wrong") {
+    import InterpreterI._
+    assertEquals(testTerm(term1), "<wrong>")
   }
 }
