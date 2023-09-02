bbc5fc05caeeeede9ff672635ae60fcf7b0c5e2b
Frank Wagner <agilecoderfrank@gmail.com>
2023-09-02 22:02:50 +0200

    introduce StackFree2 (still overflows)

---
+++ src/main/scala/StackFree2.scala
@@ -0,0 +1,46 @@
+package fpessence
+
+import scala.annotation.tailrec
+
+/*
+    See https://blog.rockthejvm.com/free-monad/
+ */
+
+object StackFreeMonad:
+  trait Monad[M[_]]:
+    def pure[A](a: A): M[A]
+    def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
+
+  object Monad:
+    def apply[M[_]](using monad: Monad[M]): Monad[M] = monad
+
+  trait ~>[F[_], G[_]]:
+    def apply[A](fa: F[A]): G[A]
+
+trait StackFree2[M[_], A]:
+  import StackFreeMonad.*
+  import StackFree2.*
+  def flatMap[B](f: A => StackFree2[M, B]): StackFree2[M, B] = FlatMap(this, f)
+  def map[B](f: A => B): StackFree2[M, B]                    = flatMap(a => pure(f(a)))
+  // @tailrec
+  final def foldMap[G[_]: Monad](natTrans: M ~> G): G[A] = this match
+    case Pure(a)     => Monad[G].pure(a)
+    case Suspend(ma) => natTrans.apply(ma)
+    case FlatMap(fa, f) => // need a G[B]
+      Monad[G].flatMap(fa.foldMap(natTrans))(a => f(a).foldMap(natTrans))
+
+object StackFree2:
+  def pure[M[_], A](a: A): StackFree2[M, A]      = Pure(a)
+  def liftM[M[_], A](ma: M[A]): StackFree2[M, A] = Suspend(ma)
+
+  case class Pure[M[_], A](a: A)                                                 extends StackFree2[M, A]
+  case class FlatMap[M[_], A, B](fa: StackFree2[M, A], f: A => StackFree2[M, B]) extends StackFree2[M, B]
+  case class Suspend[M[_], A](ma: M[A])                                          extends StackFree2[M, A]
+
+object IdMonad:
+  case class Id[A](v: A)
+
+  import StackFreeMonad.Monad
+  given idMonad: Monad[Id] with
+    def pure[A](a: A): Id[A]                           = Id(a)
+    def flatMap[A, B](ma: Id[A])(f: A => Id[B]): Id[B] = f(ma.v)
---
+++ src/test/scala/StackFreeTest2.scala
@@ -0,0 +1,72 @@
+package fpessence
+
+import munit.FunSuite
+import scala.util.Try
+import scala.util.Failure
+import scala.util.Success
+import scala.annotation.tailrec
+
+class MyStateStackFree2Test extends FunSuite:
+
+  enum MyStateF[S, +A]:
+    case Get[S, A](f: S => A)  extends MyStateF[S, A]
+    case Put[S, A](s: S, a: A) extends MyStateF[S, A]
+
+  type MyStateF_ = [S] =>> [A] =>> MyStateF[S, A]
+  given [S]: Functor[MyStateF_[S]] with
+    import MyStateF._
+    extension [A](x: MyStateF[S, A])
+      def map[B](f: A => B): MyStateF[S, B] =
+        x match
+          case Get(g)    => Get(s => f(g(s)))
+          case Put(s, a) => Put(s, f(a))
+
+  type MyStateStackFree2[S, A] = StackFree2[MyStateF_[S], A]
+
+  import StackFree2._
+  import MyStateF._
+
+  def pureMyStateStackFree[S, A](a: A): MyStateStackFree2[S, A] = pure(a)
+  def getMyStateStackFree[S]: MyStateStackFree2[S, S]           = liftM(Get(s => s))
+  def setMyStateStackFree[S](s: S): MyStateStackFree2[S, Unit]  = liftM(Put(s, ()))
+
+  import StackFreeMonad.~>
+  import IdMonad.Id
+  def myStateF2Id[S](zero: S): MyStateF_[S] ~> Id = new (MyStateF_[S] ~> Id):
+    override def apply[A](fa: MyStateF_[S][A]): Id[A] = fa match
+      case Get(g)    => Id(g(zero))
+      case Put(s, a) => Id(a)
+
+  // @tailrec
+  private def eval[S, A](s: S, x: MyStateStackFree2[S, A]): A =
+    x.foldMap(myStateF2Id(s)).v
+    // x.resume match {
+    //   case Right(value)     => value
+    //   case Left(Put(s, xx)) => eval(s, xx)
+    //   case Left(Get(f))     => eval(s, f(s))
+    // }
+
+  private def zipIndex[A](as: Seq[A]): Seq[(Int, A)] =
+    val x = as.foldLeft(
+      pureMyStateStackFree[Int, Seq[(Int, A)]](Seq())
+    )((acc, a) =>
+      for {
+        xs <- acc
+        n  <- getMyStateStackFree
+        _  <- setMyStateStackFree(n + 1)
+      } yield (n, a) +: xs
+    )
+    eval(0, x).reverse
+
+  test("zip ok"):
+    val n      = 13
+    val as     = Range.inclusive(0, n).toSeq
+    val zipped = zipIndex(as)
+    assertEquals(zipped.lastOption.map(_._1), Some(n))
+
+  test("zip without overflow"):
+    val n      = 1313131
+    val as     = Range.inclusive(0, n).toSeq
+    val zipped = Util.TryAll(zipIndex(as))
+
+    assertEquals(zipped.map(_.lastOption.map(_._1)).get, Some((n)))
